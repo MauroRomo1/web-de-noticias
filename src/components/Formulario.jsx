c9793a7f8d6efd5c1286d39ac6d63a954013240a
Mauro Romo <mauroromo.developer@outlook.com>
2023-08-02 00:13:21 -0300

    agregando un useState para el select de categorias

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,14 @@
 import { Col, Form, Row } from "react-bootstrap";
 import ListaNoticias from "./ListaNoticias";
+import { useState } from "react";
 
 const Formulario = () => {
+  const [selectValor, setselectValor] = useState("top");
+
+  const handleChange = (e) => {
+    setselectValor(e.target.value);
+  };
+
   return (
     <>
       <section className="container my-4">
@@ -10,8 +17,8 @@ const Formulario = () => {
             <Form>
               <Form.Group className="my-4" controlId="formCategoria">
                 <Form.Label>Buscar por categoria:</Form.Label>
-                <Form.Select>
-                  <option defaultValue="top">top</option>
+                <Form.Select value={selectValor} onChange={handleChange}>
+                  <option value="top">top</option>
                   <option value="business">negocios</option>
                   <option value="entertainment">entretenimiento</option>
                   <option value="food">alimentos</option>
