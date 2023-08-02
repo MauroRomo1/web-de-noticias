12aa735d6fdc2c35d2f543060bf759e754e12a70
Mauro Romo <mauroromo.developer@outlook.com>
2023-08-01 23:30:38 -0300

    terminando la estructura del componente ListaNoticias

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,31 +1,35 @@
 import { Col, Form, Row } from "react-bootstrap";
+import ListaNoticias from "./ListaNoticias";
 
 const Formulario = () => {
   return (
-    <section className="container my-4">
-      <Row className="justify-content-center">
-        <Col className="border border-3" xs="11" md="8">
-          <Form>
-            <Form.Group className="my-4" controlId="formCategoria">
-              <Form.Label>Buscar por categoria:</Form.Label>
-              <Form.Select>
-                <option defaultValue="top">top</option>
-                <option value="business">negocios</option>
-                <option value="entertainment">entretenimiento</option>
-                <option value="food">alimentos</option>
-                <option value="health">salud</option>
-                <option value="politics">política</option>
-                <option value="science">ciencia</option>
-                <option value="sports">Deportes</option>
-                <option value="technology">tecnología</option>
-                <option value="tourism">turismo</option>
-                <option value="world">mundo</option>
-              </Form.Select>
-            </Form.Group>
-          </Form>
-        </Col>
-      </Row>
-    </section>
+    <>
+      <section className="container my-4">
+        <Row className="justify-content-center">
+          <Col className="border border-3" xs="11" lg="8">
+            <Form>
+              <Form.Group className="my-4" controlId="formCategoria">
+                <Form.Label>Buscar por categoria:</Form.Label>
+                <Form.Select>
+                  <option defaultValue="top">top</option>
+                  <option value="business">negocios</option>
+                  <option value="entertainment">entretenimiento</option>
+                  <option value="food">alimentos</option>
+                  <option value="health">salud</option>
+                  <option value="politics">política</option>
+                  <option value="science">ciencia</option>
+                  <option value="sports">Deportes</option>
+                  <option value="technology">tecnología</option>
+                  <option value="tourism">turismo</option>
+                  <option value="world">mundo</option>
+                </Form.Select>
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+      </section>
+      <ListaNoticias />
+    </>
   );
 };
 
