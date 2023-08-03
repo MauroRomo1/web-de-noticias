3e7d4eefdeb9e14750558c9c356e9a0fe917bbeb
Mauro Romo <mauroromo.developer@outlook.com>
2023-08-03 01:58:16 -0300

    cambiando ek drop shadow y el bg del header

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -38,7 +38,7 @@ const Formulario = () => {
 
   return (
     <>
-      <section className="container my-4">
+      <section className="container my-5">
         <Row className="justify-content-center">
           <Col className="border border-3 bg-white shadowCard" xs="11" lg="8">
             <Form>
